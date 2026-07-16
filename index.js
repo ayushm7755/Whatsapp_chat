fb80321ef42f7189b44e734c9b324fbd8403ad0a
Ayush Mishra <ayushm7755@gmail.com>
2026-07-16 20:22:51 +0530

    remove some error

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ main().then(()=>{
 })
 .catch(err => console.log(err));
 async function main() {
-    console.log(process.env.MONGO_URI);
+    /* console.log(process.env.MONGO_URI); */
     await mongoose.connect(process.env.MONGO_URI);
 }
 
